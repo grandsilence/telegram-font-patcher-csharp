90212f8450f3372d4eb7066685d3a79ced253a99
Grand Silence <mixtape774@gmail.com>
2023-01-22 23:02:57 +0400

    :bookmark: v2.1.3

--- TelegramFontPatcher/Properties/AssemblyInfo.cs
+++ TelegramFontPatcher/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.2.0")]
-[assembly: AssemblyFileVersion("2.1.2.0")]
+[assembly: AssemblyVersion("2.1.3.0")]
+[assembly: AssemblyFileVersion("2.1.3.0")]
